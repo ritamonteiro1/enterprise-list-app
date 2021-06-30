f2e11751479c42f10f6c664149add3e5282c486a
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-30 17:00:27 -0300

    add fun findViewsById in ResultActivity.kt

--- app/src/main/java/com/example/enterprises/activity/ResultActivity.kt
+++ app/src/main/java/com/example/enterprises/activity/ResultActivity.kt
@@ -2,12 +2,26 @@ package com.example.enterprises.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.example.enterprises.R
 
 class ResultActivity : AppCompatActivity() {
+    private var resultEnterpriseImageView: ImageView? = null
+    private var resultDescriptionEnterpriseTextView: TextView? = null
+    private var resultToolBar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        findViewsById()
 
+    }
+
+    private fun findViewsById() {
+        resultEnterpriseImageView = findViewById(R.id.resultEnterpriseImageView)
+        resultDescriptionEnterpriseTextView = findViewById(R.id.resultDescriptionEnterpriseTextView)
+        resultToolBar = findViewById(R.id.resultToolBar)
     }
 }
