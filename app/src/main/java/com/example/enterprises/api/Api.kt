31e62625fe52b815c0673d063f61127d70c5e651
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-29 23:49:38 -0300

    create fun setupRetrofit in class Api

--- app/src/main/java/com/example/enterprises/api/Api.kt
+++ app/src/main/java/com/example/enterprises/api/Api.kt
@@ -1,4 +1,23 @@
 package com.example.enterprises.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class Api {
+    companion object {
+        private const val BASE_URL = "https://empresas.ioasys.com.br/api/v1/"
+        fun setupRetrofit(): Retrofit? {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client: OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create()).build()
+        }
+    }
 }
