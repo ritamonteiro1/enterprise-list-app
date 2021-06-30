f9936977eb3ebc2d4f0c0cfd9c0411cd88680340
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-29 23:56:16 -0300

    add fun recoverVerifyLogin in interface DataService.kt

--- app/src/main/java/com/example/enterprises/api/DataService.kt
+++ app/src/main/java/com/example/enterprises/api/DataService.kt
@@ -1,4 +1,12 @@
 package com.example.enterprises.api
 
+import com.example.enterprises.domains.UserRequest
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.*
+
 interface DataService {
+
+    @POST("users/auth/sign_in")
+    fun recoverVerifyLogin(@Body userRequest: UserRequest): Call<ResponseBody>
 }
