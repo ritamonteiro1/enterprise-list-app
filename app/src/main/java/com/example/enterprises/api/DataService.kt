33859e08a5ef18ac876413c47692b1d3f78cdc8e
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-30 12:00:42 -0300

    add fun recoverEnterpriseListResponse in DataService.kt

--- app/src/main/java/com/example/enterprises/api/DataService.kt
+++ app/src/main/java/com/example/enterprises/api/DataService.kt
@@ -1,5 +1,7 @@
 package com.example.enterprises.api
 
+import com.example.enterprises.constants.Constants
+import com.example.enterprises.domains.enterprise.EnterpriseListResponse
 import com.example.enterprises.domains.user.UserRequest
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -9,4 +11,12 @@ interface DataService {
 
     @POST("users/auth/sign_in")
     fun recoverVerifyLogin(@Body userRequest: UserRequest): Call<ResponseBody>
+
+    @GET("enterprises")
+    fun recoverEnterpriseListResponse(
+        @Query(Constants.ENTERPRISE_NAME) enterpriseName: String?,
+        @Header("access-token") accessToken: String?,
+        @Header("client") client: String?,
+        @Header("uid") uid: String?
+    ): Call<EnterpriseListResponse?>
 }
